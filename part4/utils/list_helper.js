48a35410c8d8c65c918d921aab9f42a2d2813f64
Xianzhi <405584993@qq.com>
2024-03-18 15:31:39 +0100

    4.3-4.4 unit test

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,14 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => {
+        return total + blog.likes
+    }, 0)
+}
+  
+module.exports = {
+    dummy,
+    totalLikes
+}
